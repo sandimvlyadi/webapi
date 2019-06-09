05e2d64eb5a9392c59ba87def385b5074fa892cd
sandimvlyadi <sandimvlyadi@gmail.com>
2019-06-09 22:11:25 +0700

    sebelum install windows 7

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.5.122/webapi/';
+// $config['base_url'] = 'http://192.168.5.122/webapi/';
 // $config['base_url'] = 'http://192.168.42.140/webapi/';
+$config['base_url'] = 'http://localhost/webapi/';
 
 /*
 |--------------------------------------------------------------------------
